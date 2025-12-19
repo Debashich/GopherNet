55e5ce27ae91d17abfc51b8b127049d97d770eaf
Debashich <debashishsinha555@gmail.com>
2025-12-19 19:50:19 +0530

    Implemented topic based filtering

--- gophernet-broker/handlers.go
+++ gophernet-broker/handlers.go
@@ -5,33 +5,46 @@ import (
 	"net/http"
 )
 
+var broker *Broker
+
 func HealthHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
 }
 
-func PublishHandler(broker *Broker) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
-			return
-		}
-
-		var e Event
-		if err := json.NewDecoder(r.Body).Decode(&e); err != nil {
-			http.Error(w, "invalid JSON", http.StatusBadRequest)
-			return
-		}
-
-		broker.AddEvent(e)
-		w.WriteHeader(http.StatusCreated)
+func PublishHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST allowed", http.StatusMethodNotAllowed)
+		return
 	}
+
+	var e Event
+	err := json.NewDecoder(r.Body).Decode(&e)
+	if err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	broker.events = append(broker.events, e)
+	w.WriteHeader(http.StatusCreated)
 }
 
-func GetEventsHandler(broker *Broker) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		events := broker.GetEvents()
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(events)
+func GetEventsHandler(w http.ResponseWriter, r *http.Request) {
+
+	topic := r.URL.Query().Get("topic")
+
+	// If no topic, return all events
+	if topic == "" {
+		json.NewEncoder(w).Encode(broker.events)
+		return
 	}
+
+	// Filter by topic
+	var filtered []Event
+	for _, e := range broker.events {
+		if e.Topic == topic {
+			filtered = append(filtered, e)
+		}
+	}
+
+	json.NewEncoder(w).Encode(filtered)
 }
--- gophernet-broker/main.go
+++ gophernet-broker/main.go
@@ -6,11 +6,12 @@ import (
 )
 
 func main() {
-	broker := NewBroker()
+
+	broker = NewBroker()
 
 	http.HandleFunc("/health", HealthHandler)
-	http.HandleFunc("/publish", PublishHandler(broker))
-	http.HandleFunc("/events", GetEventsHandler(broker))
+	http.HandleFunc("/publish", PublishHandler)
+	http.HandleFunc("/events", GetEventsHandler)
 
 	fmt.Println("GopherNet Broker running on :3000")
 	http.ListenAndServe(":3000", nil)
